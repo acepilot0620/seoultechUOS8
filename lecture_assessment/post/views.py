bbc0988b2e56ee3c2c5e1fbb669feb34c3b4add4
jonghoon <dlwhdgns0402@likelion.org>
2020-02-22 20:56:09 +0900

    post 1

--- lecture_assessment/post/views.py
+++ lecture_assessment/post/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
+from .models import Post
+from account.models import Profile
 
 # Create your views here.
+
+def post(request):
+
+    if request.method == "POST":
+        post = Post()
+        try: #로그인 체크
+            user = request.user
+            profile = Profile.objects.get(user=user)
+            post.author = profile
+            post.prof = request.POST['prof']
+            post.term = request.POST['term']
+            post.content = request.POST['prof']
+            post.total_score = request.POST['prof']
+            post.check_att = request.POST['attend']
+            post.lev_of_diff = request.POST['Level']
+            post.quantity = request.POST['Study']
+            post.grade = request.POST['Grade']
+            post.achievement = request.POST['Value']
+            post.save()
+        except: #유저 로그인 안 했을 경우
+            return redirect('account:login')
+        return redirect('home')
+    else:
+        try: #로그인 체크
+            user = request.user
+            profile = Profile.objects.get(user=user)
+        except: #유저 로그인 안했을 경우
+            return redirect('account:login')
+        return render(request, 'post/post.html')
