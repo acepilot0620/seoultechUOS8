1c1385011e87614f5f0dac7845852a1173c169bb
acepilot0620 <49195475+acepilot0620@users.noreply.github.com>
2020-02-22 20:53:15 +0900

    delete venv

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\acepi\desktop\seoultechuos8\seoultechuos8\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
